dad95345d81d96c01b2c90efbb43e4c552ba0aae
sigzen-fork <sigzenbackup@gmail.com>
2025-10-11 17:54:38 +0530

    Update hooks.py

--- india_compliance/hooks.py
+++ india_compliance/hooks.py
@@ -6,7 +6,7 @@ app_icon = "octicon octicon-file-directory"
 app_color = "grey"
 app_email = "hello@indiacompliance.app"
 app_license = "GNU General Public License (v3)"
-required_apps = ["frappe/erpnext"]
+required_apps = ["erpnext"]
 
 before_install = "india_compliance.patches.check_version_compatibility.execute"
 after_install = "india_compliance.install.after_install"
